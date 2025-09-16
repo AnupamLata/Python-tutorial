7936e20b6913fef4d8c1c76e79c5d591b09f18e5
Anupam Lata Yadav <yanu77662@gmail.com>
2025-09-16 16:03:28 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,16 @@
+import pyjokes
+
+print("printing jokes..")
+
+# this is a random joke
+
+joke = pyjokes.get_joke()
+print(joke)
+
+"""so thanks
+that was my program"""
+
+# import calendar
+# yy = 2025
+# mm = 8
+# print(calendar.month(yy,mm))
